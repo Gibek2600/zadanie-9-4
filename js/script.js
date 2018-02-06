5095e7543a4c5e6a41879e8ae770c9a805d1e227
Michał Gibczyński <Gibek2600@interia.pl>
2018-02-06 18:35:55 +0100

    add js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function drawTree(h) {
+	for ( var i = 0 ; i < h ; i++) {
+		var star = '';
+		//var space = '';
+		for (var j = 0 ; j <= i ; j++){
+			star += ' *';
+		}
+		/*for (var p = h ; p <= i ; p--){
+			space += ' ';
+		}*/
+		
+	console.log(star);
+
+	}
+}
